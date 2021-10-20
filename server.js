66242108b490a4ddd918e06c2b23d4ec138007f1
Paul Frazier <81392028+PxFrazier@users.noreply.github.com>
2021-10-20 13:10:18 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static('public'));
+app.use('/styles', express.static(__dirname + 'public/styles'));
+
+app.get('/', (req, res)=>{
+    res.render('login');
+});
+
+app.post('/login', (req, res)=>{
+    let user_name = req.body.employee;
+    let password = req.body.password;
+    res.render('./main/index', {employee: user_name, password: password});
+});
+
+app.listen(PORT, ()=>{
+    console.log(`App started on port: ${PORT}`);
+});

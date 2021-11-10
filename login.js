86f6e5071d9e638181badc1dd8db8e7bf3f15db2
Paul Frazier <81392028+PxFrazier@users.noreply.github.com>
2021-11-10 10:27:57 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require('mysql');
+const router = express.Router();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Onkwehonwe+1',
+    database: 'microblast_arts'
+});
+
+connection.connect((err)=>{
+    if(err) throw err;
+    console.log('Database connection established.');
+});
+
+router.post('/', (req, res)=>{
+    let sql_query = `SELECT * FROM employees WHERE EMP_ID = \"${req.body.employee_ID}\" AND EMP_PASSWORD = \"${req.body.password}\"`;
+    
+    connection.query(sql_query, (err, result)=>{
+        if(err) throw err;
+
+        result.length == 0 ?
+        res.redirect('/') :
+        (function(){
+            let rows = JSON.parse(JSON.stringify(result[0]));
+            res.render('./main/index', {employee: `${rows.EMP_FIRST_NAME} ${rows.EMP_LAST_NAME}`, email: rows.EMP_EMAIL});
+        }());
+    });
+});
+
+module.exports = router;
